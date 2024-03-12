2029b91e52e730e30b930b33af6154091e34a9ed
Moonm4nn <mca@njit.edu>
2024-03-11 22:32:20 -0400

    Added basic if statement

--- pyBasics.py
+++ pyBasics.py
@@ -13,5 +13,11 @@ def greetings(name_list, room_num):
     for name in name_list:
         print(f"Welcome {name}")
         print(f"You are in room {room_num}")
+    print(f"There are {len(name_list)} people in this room")
+
+    if len(name_list) < 5:
+        print("You are in a small room")
+    else:
+        print("You are in a normal room")
 
 greetings(names, 3)
